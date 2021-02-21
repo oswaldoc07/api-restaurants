ef95eee4cbd94eaaa81b6717f3a87e3e876f6003
oswaldoc07 <oswaldoc07@gmail.com>
2021-02-20 23:01:27 -0600

    se trabaja en guardar orden

--- src/main/java/com/ecomers/api/restaurants/web/controller/CommerceController.java
+++ src/main/java/com/ecomers/api/restaurants/web/controller/CommerceController.java
@@ -69,7 +69,7 @@ public class CommerceController {
     @GetMapping("/{id}/rate/{distance}")
     public ResponseEntity<Double> getRate(@PathVariable("id") int id,
                                           @PathVariable("distance") double distance  ) {
-        return service.getRate(id, distance)
+        return service.getRate(id, distance*1000)
                 .map(rate -> new ResponseEntity<>(rate, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
