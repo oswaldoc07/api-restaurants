926b20f22e9d11b50788cc84cb521b080a4e32a9
oswaldoc07 <73155138+oswaldoc07@users.noreply.github.com>
2021-06-24 14:17:38 -0600

    se agrega tipo de comercio

--- src/main/java/com/ecommerce/api/restaurants/persistence/entity/TipoComercio.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.api.restaurants.persistence.entity;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "categoria_producto")
-public class TipoComercio {
-}
