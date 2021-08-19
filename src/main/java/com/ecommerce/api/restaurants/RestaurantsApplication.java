34ff8dfe02f0562df96aae3f6643c886ff5e9ad0
ocortes <oswaldoc07@gmail.com>
2021-08-18 21:58:33 -0600

    se configura base de datos de debian

--- src/main/java/com/ecommerce/api/restaurants/RestaurantsApplication.java
+++ src/main/java/com/ecommerce/api/restaurants/RestaurantsApplication.java
@@ -3,13 +3,21 @@ package com.ecommerce.api.restaurants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class RestaurantsApplication {
+public class RestaurantsApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantsApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder builder) {
+		return builder.sources(RestaurantsApplication.class);
 	}
 
 }
